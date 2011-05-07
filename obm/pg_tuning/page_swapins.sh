1aaaf93ca2f5ec52612ed75760bdf536f1f42d24
thebigspoon <gregcorradini@gmail.com>
2011-05-06 18:41:29 -0700

    bad script (i'll tweak it later) for monitoring page swapins

---
+++ obm/pg_tuning/page_swapins.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+TIMESTAMP=$(date)
+echo ""
+echo ""
+echo "####################################################################"
+echo "####################################################################"
+echo "SAMPLE @ >>> " $TIMESTAMP
+echo "####################################################################"
+echo "####################################################################"
+echo ""
+echo ""
+echo "######## vmstat output #########################"
+vmstat 1 4
+echo "######## process tree for user postgres  #######"
+#ps -ALF | sort -nr -k 4 | head -30
+ps -U postgres -H
+echo "######## process tree -F user postgres #########"
+ps -U postgres -jHF
+echo "######## thread listing for user postgres ######"
+#ps -ALF | sort -nr -k 3 | head -30
+ps -U postgres -LF
+echo "######## process size/thread cnt postgres ######"
+ps -U postgres -o pid,nlwp,vsz,args
+echo "######## free output by MB  ####################"
+free -m
+echo "######## loading average #######################"
+uptime
+
